62a0d340d471af584b0ffe548046dce579e9f1a3
zakmatt <zak.matthew@yahoo.com>
2018-08-01 18:37:06 -0400

    generate new batch of images for validation after each epoch, improve checkpoint class

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 import argparse
-
+import numpy as np
 
 from keras.callbacks import ModelCheckpoint, Callback
 from keras.optimizers import Adam
 
-#from networks.unet_mask_out import Unet
+# from networks.unet_mask_out import Unet
 from networks.unet import Unet
 
 from utils.batch_generator import BatchGenerator
@@ -16,10 +16,8 @@ NB_EPOCHS = 1001
 
 
 class LossValidateCallback(Callback):
-
-    def __init__(self, train_batch, val_batch, results_file):
-        self.train_batch = train_batch
-        self.val_batch = val_batch
+    def __init__(self, batch_generator, results_file):
+        self.batch_generator = batch_generator
 
         import os
         basedir = os.path.dirname(results_file)
@@ -27,14 +25,53 @@ class LossValidateCallback(Callback):
             os.makedirs(basedir)
         self.results_file = results_file
 
+    @staticmethod
+    def IOU_loss(y_true, y_false):
+        def IOU_calc(y_true, y_false, smooth=1.):
+            y_true_f = y_true.flatten()
+            y_false_f = y_false.flatten()
+            intersection = np.sum(y_true_f * y_false_f)
+            return 2 * (intersection + smooth) / (
+                np.sum(y_true_f) + np.sum(y_false_f) + smooth)
+
+        return 1 - IOU_calc(y_true, y_false)
+
     def on_epoch_end(self, epoch, logs=None):
-        train_imgs, train_masks = self.train_batch
-        val_imgs, val_masks = self.val_batch
-        train_loss, _ = self.model.evaluate(train_imgs, train_masks)
-        val_loss, _ = self.model.evaluate(val_imgs, val_masks)
-        text = 'epoch: {0} train_loss: {1}, validation loss: {2}\n'.format(
-            epoch, train_loss, val_loss
+        # train_losses, val_losses = []
+        train_batch, val_batch = self.batch_generator(VAL_BATCH)
+        train_imgs, train_masks = train_batch
+        val_imgs, val_masks = val_batch
+        train_results = self.model.predict(train_imgs)
+        val_results = self.model.predict(val_imgs)
+
+        train_losses = [LossValidateCallback.IOU_loss(*pair) for pair in
+                        zip(train_masks, train_results)]
+        average_train_loss = np.average(train_losses)
+        std_train_loss = np.std(train_losses)
+        val_losses = [
+            LossValidateCallback.IOU_loss(*pair) for pair in
+            zip(val_masks, val_results)]
+        average_val_loss = np.average(val_losses)
+        std_val_loss = np.std(val_losses)
+
+        batch_train_loss = LossValidateCallback.IOU_loss(train_masks,
+                                                         train_results)
+        batch_val_loss = LossValidateCallback.IOU_loss(val_masks, val_results)
+
+        eval_train_loss, _ = self.model.evaluate(train_imgs, train_masks)
+        eval_val_loss, _ = self.model.evaluate(val_imgs, val_masks)
+
+        text = 'epoch: {0} evaluation: train_loss: {1}, ' \
+               'validation loss: {2}, '.format(
+                   epoch, eval_train_loss, eval_val_loss)
+        text += 'batch eval: train loss: {0} validation loss: {1}, '.format(
+            batch_train_loss, batch_val_loss
         )
+        text += 'batch stats: train avg loss: {0}, train std loss: {1}, ' \
+                'val avg loss: {2}, val std loss: {3}\n'.format(
+                    average_train_loss, std_train_loss,
+                    average_val_loss, std_val_loss
+                )
         with open(self.results_file, 'a') as file:
             file.writelines(text)
 
@@ -63,7 +100,7 @@ def train(data_dir, val_data_dir, results_file):
         callbacks=[
             checkpoint,
             LossValidateCallback(
-                *batch_gen.generate_test_batch(VAL_BATCH),
+                batch_gen.generate_test_batch,
                 results_file
             )
         ],
