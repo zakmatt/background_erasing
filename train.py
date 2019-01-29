cf222236d25cf952a7f525de80f10973e1851ad0
zakmatt <zak.matthew@yahoo.com>
2019-01-28 22:32:29 -0500

    fix problems with train.py

--- train.py
+++ train.py
@@ -1,22 +1,25 @@
 import argparse
 import os
+import pandas as pd
 
 from networks.unet import Unet
 
 from utils.batch_generator import BatchGenerator
 
 BATCH_SIZE = 1
-VAL_BATCH = 150
 IMG_ROWS, IMG_COLS = 256, 256
-NB_EPOCHS = 1000
-STEPS_PER_EPOCH = 1e3
+NB_EPOCHS = 500
+STEPS_PER_EPOCH = 500
+
+LUNGS_IMGS = './dataset/lungs_segmentation/lungs_resized/'
+LUNGS_MASKS = './dataset/lungs_segmentation/masks_dilated_resized/'
 
 
-def train(data, validation, results_file,
-          save_model_dir, steps_per_epoch, initial_epoch=0):
+def train(data_path, validation, results_file,
+          save_model_dir, steps_per_epoch=STEPS_PER_EPOCH, initial_epoch=0):
     """Model training main script
 
-    :param data: dataset
+    :param data: data_path
     :param validation: Dataset split portion for validation
     :type validation: float
     :param results_file: name of a file where the results are to be stored
@@ -27,11 +30,22 @@ def train(data, validation, results_file,
     :return:
     """
 
+    data = pd.read_csv(data_path)
+    data['image_path'] = [
+        '{}{}'.format(LUNGS_IMGS, file_name) for
+        file_name in data.file_name.values
+    ]
+    data['mask_path'] = [
+        '{}{}'.format(LUNGS_MASKS, file_name) for
+        file_name in data.file_name.values
+    ]
+    val_batch_size = int(len(data_path) * validation)
+
     batch_gen = BatchGenerator(
         data=data, validate=validation, batch_size=BATCH_SIZE
     )
     model = Unet(IMG_ROWS, IMG_COLS, batch_gen,
-                 save_model_dir, results_file, VAL_BATCH)
+                 save_model_dir, results_file, val_batch_size)
 
     if initial_epoch > 0:
         model.load_weights(initial_epoch)
@@ -42,12 +56,12 @@ def train(data, validation, results_file,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-d",
-                        "--data_dir",
-                        help='Training data directory',
+                        "--data_path",
+                        help='Training data info csv file',
                         required=True)
     parser.add_argument("-v",
-                        "--val_dir",
-                        help='Validation data directory',
+                        "--val",
+                        help='Validation split',
                         required=True)
     parser.add_argument("-r",
                         "--results_file",
@@ -62,8 +76,8 @@ if __name__ == '__main__':
                         help='Save model dir',
                         required=False)
     args = parser.parse_args()
-    data_dir = args.data_dir
-    val_dir = args.val_dir
+    data_path = args.data_path
+    val = float(args.val)
     results_file = args.results_file
     initial_epoch = args.initial_epoch
     save_model_dir = args.save_model_dir
@@ -79,6 +93,6 @@ if __name__ == '__main__':
 
     if initial_epoch:
         initial_epoch = int(initial_epoch)
-        train(data_dir, val_dir, results_file, save_model_dir, initial_epoch)
+        train(data_path, val, results_file, save_model_dir, initial_epoch)
     else:
-        train(data_dir, val_dir, results_file, save_model_dir)
+        train(data_path, val, results_file, save_model_dir)
