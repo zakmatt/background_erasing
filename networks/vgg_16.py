c43906068366c5539c1a73e880600dc304605509
zakmatt <zak.matthew@yahoo.com>
2019-03-11 22:35:44 -0400

    create vgg_16 based network to classify lungs

---
+++ networks/vgg_16.py
@@ -0,0 +1,101 @@
+import os
+
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.applications.vgg16 import VGG16
+from keras.layers import (
+    Dense,
+    GlobalAveragePooling2D,
+    BatchNormalization,
+    Conv2D
+)
+from keras.models import Model
+from keras.optimizers import Adam
+from utils.loss_validate_callback import LossValidateCallback
+
+
+# noinspection PyPep8Naming
+class VGG16_N(object):
+    """
+
+    """
+
+    def __init__(self, img_rows, img_cols, batch_gen,
+                 save_model_dir, results_file):
+
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        self.batch_gen = batch_gen
+        self.model = VGG16_N.model(img_rows=img_rows, img_cols=img_cols)
+        self.model.compile(
+            optimizer=Adam(lr=1e-4),
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+
+        f_path = str(
+            save_model_dir
+        ) + '/vgg16_batch_' + str(
+            batch_gen.batch_size
+        ) + '_epoch_{epoch:02d}.hdf5'
+
+        checkpoint = ModelCheckpoint(
+            filepath=f_path,
+            mode='auto',
+            monitor='val_loss',
+            save_best_only=True,
+        )
+        self.callbacks = [checkpoint]
+
+        reduce_on_plateau = ReduceLROnPlateau(
+            monitor='val_loss', factor=0.8, patience=10, verbose=1,
+            mode='auto', min_delta=0.0001, cooldown=5, min_lr=0.0001
+        )
+
+        results_file = os.path.join(save_model_dir, results_file)
+        self.callbacks = [
+            checkpoint,
+            reduce_on_plateau,
+            LossValidateCallback(
+                batch_gen.generate_test_batch,
+                results_file
+            )
+        ]
+
+    @staticmethod
+    def model(img_rows, img_cols):
+        base_model = VGG16(
+            input_shape=(img_rows, img_cols, 3),
+            weights='imagenet',
+            include_top=False
+        )
+        base_model.trainable = False
+
+        # add a global spatial average pooling layer
+        x = base_model.output
+        x = BatchNormalization()(x)
+        x = Conv2D(
+            64, kernel_size=(1, 1), padding='same',
+            activation='relu', name='last_conv'
+        )(x)
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(256, activation='relu')(x)
+        predictions = Dense(3, activation='softmax', name='activations')(x)
+
+        model = Model(inputs=base_model.input, outputs=predictions)
+
+        return model
+
+    def train(self, initial_epoch, nb_epochs, steps_per_epoch):
+
+        (_, _), (val_x, val_y) = self.batch_gen.generate_test_batch()
+        self.model.fit_generator(
+            self.batch_gen.train_batches,
+            steps_per_epoch=steps_per_epoch,
+            epochs=nb_epochs,
+            callbacks=self.callbacks,
+            initial_epoch=initial_epoch,
+            validation_data=(val_x, val_y)
+        )
+
+    def load_weights(self, weights_path):
+        self.model.load_weights(weights_path)
---
+++ train_classification.py
@@ -0,0 +1,96 @@
+import argparse
+import os
+import pandas as pd
+
+from keras.utils import to_categorical
+
+from networks.vgg_16 import VGG16_N
+from utils.batch_generator import BatchGenerator
+
+BATCH_SIZE = 8
+IMG_SHAPE = (256, 256, 3)
+NB_EPOCHS = 500
+STEPS_PER_EPOCH = 300
+
+LUNGS_DIR = './dataset/final_classification/'
+
+
+def train(data_path, validation, results_file,
+          save_model_dir, steps_per_epoch=STEPS_PER_EPOCH, initial_epoch=0):
+    """Model training main script
+
+    :param data: data_path
+    :param validation: Dataset split portion for validation
+    :type validation: float
+    :param results_file: name of a file where the results are to be stored
+    :param save_model_dir: path to a place where the results are to be saved
+    :param model_info: information whether we start training from a particular
+    epoch and with pretrained weights
+    :type model_info: dict
+    :return:
+    """
+
+    data = pd.read_csv(data_path)
+    data['image_path'] = data.file.apply(
+        lambda x: os.path.join(LUNGS_DIR, x))
+    data['class'] = list(to_categorical(data['class']))
+
+    batch_gen = BatchGenerator(
+        data=data, validate=validation,
+        batch_size=BATCH_SIZE, segmentation=False,
+        shape=IMG_SHAPE[:-1]
+    )
+    model = VGG16_N(
+        IMG_SHAPE[0], IMG_SHAPE[1], batch_gen,
+        save_model_dir, results_file
+    )
+
+    if initial_epoch > 0:
+        model.load_weights(initial_epoch)
+
+    model.train(initial_epoch, NB_EPOCHS, steps_per_epoch)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d",
+                        "--data_path",
+                        help='Training data info csv file',
+                        required=True)
+    parser.add_argument("-v",
+                        "--val",
+                        help='Validation split',
+                        required=True)
+    parser.add_argument("-r",
+                        "--results_file",
+                        help='Metrics results file path',
+                        required=True)
+    parser.add_argument("-e",
+                        "--initial_epoch",
+                        help='Initial epoch #',
+                        required=False)
+    parser.add_argument("-s",
+                        "--save_model_dir",
+                        help='Save model dir',
+                        required=False)
+    args = parser.parse_args()
+    data_path = args.data_path
+    val = float(args.val)
+    results_file = args.results_file
+    initial_epoch = args.initial_epoch
+    save_model_dir = args.save_model_dir
+
+    if not save_model_dir:
+        save_model_dir = '.'
+    else:
+        save_model_dir = str(save_model_dir)
+        if save_model_dir[-1] == '/':
+            save_model_dir = save_model_dir[:-1]
+        if not os.path.isdir(save_model_dir):
+            os.makedirs(save_model_dir)
+
+    if initial_epoch:
+        initial_epoch = int(initial_epoch)
+        train(data_path, val, results_file, save_model_dir, initial_epoch)
+    else:
+        train(data_path, val, results_file, save_model_dir)
