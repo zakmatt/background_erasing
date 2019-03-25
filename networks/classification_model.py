ce8a96eb39315797ff87fe37e3981386158e6167
zakmatt <zak.matthew@yahoo.com>
2019-03-25 09:59:51 -0400

    add 3 dense layers on top of pretrained networks

--- networks/classification_model.py
+++ networks/classification_model.py
@@ -18,6 +18,8 @@ class ClassModel(metaclass=ABCMeta):
 
     """
 
+    name = 'class_model'
+
     def __init__(self, img_rows, img_cols, batch_gen,
                  save_model_dir, results_file):
 
@@ -27,7 +29,7 @@ class ClassModel(metaclass=ABCMeta):
 
         f_path = str(
             save_model_dir
-        ) + '/vgg16_batch_' + str(
+        ) + '/' + self.name + '_batch_' + str(
             batch_gen.batch_size
         ) + '_epoch_{epoch:02d}.hdf5'
 
@@ -76,6 +78,8 @@ class ClassModel(metaclass=ABCMeta):
         # add a global spatial average pooling layer
         x = base_model.output
         x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+        x = Dense(512, activation='relu')(x)
         x = Dense(256, activation='relu')(x)
         predictions = Dense(3, activation='softmax', name='activations')(x)
 
